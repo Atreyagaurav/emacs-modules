5e1bca9b80b24e7c2fd2000e71c88969e9481909
Gaurav <allmanpride@gmail.com>
2021-10-17 00:47:22 -0400

    rename variables to calt from calc

--- calc-mode.el
+++ calc-mode.el
@@ -1,101 +1,130 @@
+;;; calt-mode.el --- Minor mode for calculation based templating.  -*- lexical-binding: t; -*-
 
-(setq calc-subprocess-engine "python")
-(setq calc-subprocess-arg "-i")
+;; Copyright (C) 2021  
 
-(defun calc-filter-func (proc string)
+;; Author: Gaurav Atreya <allmanpride@gmail.com>
+;; Version: 0.1
+;; Keywords: calculator, templating, LaTeX
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+(setq calt-subprocess-engine "python")
+(setq calt-subprocess-arg "-i")
+
+(defun calt-filter-func (proc string)
   (internal-default-process-filter proc string)
   (setq string (if (string-match ">>>" string)
 		   (string-trim-right string "[ >\n]*")
 		 (string-trim-right string)))
-  (setq calc-last-eval-string
-	 (concat calc-last-eval-string string)))
+  (setq calt-last-eval-string
+	 (concat calt-last-eval-string string)))
 
 
-(defun calc-start-process ()
+(defun calt-start-process ()
   (interactive)
-  (calc-clear-last-eval-string)
-  (setq calc-subprocess-process (start-process
-				 (concat "calc-" calc-subprocess-engine)
-				 (concat "*calc-" calc-subprocess-engine "*")
-				 calc-subprocess-engine
-				 calc-subprocess-arg
+  (calt-clear-last-eval-string)
+  (setq calt-subprocess-process (start-process
+				 (concat "calt-" calt-subprocess-engine)
+				 (concat "*calt-" calt-subprocess-engine "*")
+				 calt-subprocess-engine
+				 calt-subprocess-arg
 				 ))
   
-  (set-process-filter calc-subprocess-process 'calc-filter-func)
+  (set-process-filter calt-subprocess-process 'calt-filter-func)
   )
 
-(defun calc-clear-last-eval-string ()
-  (setq calc-last-eval-string ""))
+(defun calt-clear-last-eval-string ()
+  (setq calt-last-eval-string ""))
 
 
-(defun calc-eval-string (string)
-  (calc-clear-last-eval-string)
-  (calc-send-string-to-buffer string)
-  (process-send-string calc-subprocess-process (concat string "\n"))
-  (accept-process-output calc-subprocess-process 2)
-  calc-last-eval-string
+(defun calt-eval-string (string)
+  (calt-clear-last-eval-string)
+  (calt-send-string-to-buffer string)
+  (process-send-string calt-subprocess-process (concat string "\n"))
+  (accept-process-output calt-subprocess-process 2)
+  calt-last-eval-string
   )
 
-(defun calc-send-string-to-buffer (string)
-    (with-current-buffer (process-buffer calc-subprocess-process)
-      (goto-char (process-mark calc-subprocess-process))
+(defun calt-send-string-to-buffer (string)
+    (with-current-buffer (process-buffer calt-subprocess-process)
+      (goto-char (process-mark calt-subprocess-process))
       (insert (concat string "\n"))
-      (set-marker (process-mark calc-subprocess-process) (point))
-      (goto-char (process-mark calc-subprocess-process))
+      (set-marker (process-mark calt-subprocess-process) (point))
+      (goto-char (process-mark calt-subprocess-process))
       )
     )
 
 
-(defun calc-eval-region (beg end)
+(defun calt-eval-region (beg end)
   (interactive (if (use-region-p)
                    (list (region-beginning) (region-end))
                  (list (point-min) (point-min))))
-  (calc-clear-last-eval-string)
+  (calt-clear-last-eval-string)
   (message "Evaluating Expression. C-g to cancel.")
-  (calc-send-string-to-buffer (buffer-substring-no-properties beg end))
-  (process-send-region calc-subprocess-process beg end)
-  (process-send-string calc-subprocess-process "\n")
-  (accept-process-output calc-subprocess-process 2)
-  (message calc-last-eval-string)
+  (calt-send-string-to-buffer (buffer-substring-no-properties beg end))
+  (process-send-region calt-subprocess-process beg end)
+  (process-send-string calt-subprocess-process "\n")
+  (accept-process-output calt-subprocess-process 2)
+  (message calt-last-eval-string)
   )
 
-(defun calc-eval-and-replace-region (beg end)
+(defun calt-eval-and-replace-region (beg end)
   (interactive (if (use-region-p)
                    (list (region-beginning) (region-end))
                  (list (point-min) (point-min))))
-  (calc-eval-region beg end)
+  (calt-eval-region beg end)
   (kill-region beg end)
-  (insert calc-last-eval-string)
+  (insert calt-last-eval-string)
   )
 
-(defun calc-parse-template (string)
+(defun calt-parse-template (string)
   (let ((res "") (expression "") (fmt "%s"))
     (if (match-string 2 string)
       (let* ((expression (match-string 1 string))
-	(res (calc-eval-string expression))
+	(res (calt-eval-string expression))
 	(fmt (match-string 2 string)))
-	(if (string-match-p "[0-9.]*[df]" fmt)
-	    (setq res (string-to-number res)))
+	(if (string-match-p "[0-9.]*[dfex]" fmt)
+	    (setq res (string-to-number res))
+	  (setq res (replace-quote res)))
 	(format (concat "%" fmt) res))
-      (calc-eval-string (match-string 1 string)))))
+      (replace-quote (calt-eval-string (match-string 1 string))))))
 
-(defun calc-eval-template (beg end)
+(defun calt-eval-template (beg end)
   (interactive (if (use-region-p)
                    (list (region-beginning) (region-end))
-                 (list (point-min) (point-min))))
+                 (let ((bnd (bounds-of-thing-at-point 'line)))
+		   (list (first bnd) (rest bnd)))))
   (let ((region (buffer-substring-no-properties beg end)) newln)
     (setq newln (replace-regexp-in-string
 		 "<<\\(.*?\\);\\([0-9.a-zA-Z]+\\)?>>"
-		 'calc-parse-template region))
+		 'calt-parse-template region))
     (message "%s" newln)
   ))
 
-(defun calc-eval-and-insert-template (beg end)
+(defun calt-eval-and-insert-template (beg end)
   (interactive (if (use-region-p)
                    (list (region-beginning) (region-end))
-                 (list (point-min) (point-min))))
+                 (let ((bnd (bounds-of-thing-at-point 'line)))
+		   (list (first bnd) (rest bnd)))))
   (save-excursion
-    (let ((newln (calc-eval-template beg end)) tmp)
+    (let ((newln (calt-eval-template beg end)) tmp)
       (when (> (length (string-trim newln)) 0)
 	(goto-char (max beg end))
 	(insert "\n")
@@ -105,11 +134,40 @@
 	(insert "\n")))
     ))
 
-(defun calc-kill-process ()
+(defun calt-kill-process ()
   (interactive)
-  (kill-process (concat "calc-" calc-subprocess-engine))
+  (kill-process (concat "calt-" calt-subprocess-engine))
   )
 
-(global-set-key (kbd "C-e") 'calc-eval-and-replace-region)
-(global-set-key (kbd "C-S-e") 'calc-eval-region)
+(defun calt-format-region (beg end)
+    (interactive (if (use-region-p)
+                   (list (region-beginning) (region-end))
+                 (let ((bnd (bounds-of-thing-at-point 'symbol)))
+		   (list (first bnd) (rest bnd)))))
+    (let ((fmt (concat
+		  "%"
+		  (read-string "Enter format string: %")))
+	  (text (buffer-substring-no-properties beg end)))
+      (if (string-match-p "%[0-9.]*[dfex]" fmt)
+	    (setq text (string-to-number text)))
+      (kill-region beg end)
+      (insert (format fmt text)))
+    )
 
+
+(define-minor-mode calt-mode
+  "Minor mode for Calculation based templating."
+  :lighter " Calt"
+  :keymap `(
+	    (,(kbd "p") . calt-start-process)
+	    (,(kbd "q") . calt-stop-process)
+	    (,(kbd "T") . calt-eval-template)
+	    (,(kbd "t") . calt-eval-and-insert-template)
+	    (,(kbd "r") . calt-eval-and-replace-region)
+	    (,(kbd "R") . calt-eval-region)
+	    (,(kbd "f") . calt-format-region)
+	    ))
+
+
+(provide 'calt-mode)
+;;; calt-mode.el ends here
